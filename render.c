538f455c52caa5ecc50ca3bb4c873a80ae8ef1e7
Eric Fischer <enf@pobox.com>
2013-05-31 18:13:43 -0700

    Dot brightness ramp: looks better for census, still OK for GPS dots

--- render.c
+++ render.c
@@ -49,7 +49,7 @@ void process(char *fname, int components, int z_lookup, unsigned char *startbuf,
 	}
 
 	int step = 1, brush = 1;
-	double bright = .015;
+	double bright = .1;
 	if (components == 1) {
 #define ALL 13
 		if (z_draw >= ALL) {
@@ -59,7 +59,7 @@ void process(char *fname, int components, int z_lookup, unsigned char *startbuf,
 			step = 1 << (ALL - z_draw);
 		}
 
-		bright *= exp(log(1.15) * (z_draw - 5));
+		bright *= exp(log(1.4) * (z_draw - 15));
 	} else {
 		bright = 0.075; // looks good at zoom level 5
 
